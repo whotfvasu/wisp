8af3bce08850458736c6fc59f42cf4840036fae9
Vasu Parashar <136962787+whotfvasu@users.noreply.github.com>
2025-06-19 19:08:45 +0530

    signup controller added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,39 @@
-
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 //signup new user
 export const signup = async (req, res) => {
-    
-}
+  const { fullName, email, password, bio } = req.body;
+  try {
+    if (!fullName || !email || !password || !bio) {
+      return res.json({ success: false, message: "missing details" });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.json({ success: false, message: "account already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+      bio,
+    });
+
+    const token = generateToken(newUser._id);
+    res.json({
+      success: true,
+      userData: newUser,
+      token,
+      message: "Account created successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
-    fullname: { type: String, required: true },
+    fullName: { type: String, required: true },
     password: { type: String, required: true, minlength: 6 },
     profilePic: { type: String, default: "" },
     bio: { type: String },
