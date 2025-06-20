76e11b06b3fb6a7e74adab6c68f491c8112d0524
Vasu Parashar <136962787+whotfvasu@users.noreply.github.com>
2025-06-20 18:44:38 +0530

    navigation routes fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 const App = () => {
+  const { authUser } = useContext(AuthContext);
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
-      <Toaster/>
+      <Toaster />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
